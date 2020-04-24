64541f863610dec355ab9ce0fb2e98166e1a90f6
celikemir <celikemirhan6@gmail.com>
2020-04-24 21:50:03 +0300

    contrail for single point

---
+++ contrail.py
@@ -0,0 +1,30 @@
+import cv2
+from collections import deque
+
+class CenterPoint:
+    pts = deque(maxlen=2)
+    
+    def __init__(self,frame,mainframe,startX,startY,endX,endY):
+        self.frame = frame
+        self.mainframe = mainframe
+        self.startX = startX
+        self.startY = startY
+        self.endX = endX
+        self.endY = endY
+
+
+    def getpoint(self):
+        contours , hierarchy = cv2.findContours(self.frame,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        center = None
+
+        c = max(contours, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        self.pts.appendleft(center)
+        #print(self.pts)
+
+        cv2.circle(self.mainframe[self.startY:self.endY, self.startX:self.endX], center, 5, (0, 0, 0), -1)
+
+        return center
+
---
+++ cropsection.py
@@ -0,0 +1,29 @@
+import cv2
+
+
+class cropregion:
+    def __init__(self,frame,mainframe):
+        self.frame = frame
+        self.mainframe = mainframe
+        self.roi_x=0
+        self.roi_y=0
+        self.roi_w=0
+        self.roi_h=0
+
+
+    def select(self):
+       
+        ROI = cv2.selectROI("Alan Secimi", self.frame, False, False)
+        self.roi_x = ROI[0]
+        self.roi_y = ROI[1]
+        self.roi_w = ROI[2]
+        self.roi_h = ROI[3]
+
+        cv2.destroyAllWindows()
+       
+        selected_region = [self.roi_x, self.roi_y, self.roi_w, self.roi_h]
+        cv2.rectangle(self.mainframe, (selected_region[0], selected_region[1]),(selected_region[0] + selected_region[2], selected_region[1] + selected_region[3]), (0, 0, 255), 2)
+
+        return selected_region
+
+    
